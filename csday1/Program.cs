32c1ba74e635c1285d165f3ed4fdbb8d1927d2ac
corymccormick <76664167+corymccormick@users.noreply.github.com>
2021-05-23 21:58:28 -0600

    Add files via upload

---
+++ csday1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csday1
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello There!");
+      int MyFavNum = 7 + 6;
+      Console.WriteLine(MyFavNum);
+      string myFavoriteColor = "black";
+
+
+
+
+    }
+  }
+}
